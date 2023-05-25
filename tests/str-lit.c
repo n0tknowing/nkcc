fc7da7d659964de35f4b20b7cd3098428c0055e3
Widianto Nur F <xnaltasee@gmail.com>
2023-05-25 22:49:00 +0700

    tests/str-lit: add more literals

--- tests/str-lit.c
+++ tests/str-lit.c
@@ -1,7 +1,8 @@
 int main(void)
 {
-    const char *a = "hello
-        ";
-
+    const char *a = "\"hello\n\n\n\n\r\"";
+    const char *b = "";
+    const char *c = "helllo
+        world";
     return a[0] == 'a';
 }
