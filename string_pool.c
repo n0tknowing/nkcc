61da936c4713346562ac4b7eecf370317ea95fda
Widianto Nur F <xnaltasee@gmail.com>
2024-01-26 22:46:47 +0700

    Tidying string_pool.c
    Why I wrote the code like this?

--- string_pool.c
+++ string_pool.c
@@ -1,3 +1,30 @@
+/*
+ *  How the string pool (or string interning) works:
+ *
+ *  The implementation is very simple, each string_ref is actually an index to
+ *  the `struct pool_array`, where index 0 is reserved for empty string.
+ *  Its struct data, which is `struct string_off` stores information about a
+ *  string_ref. The information needed are just the offset to the `struct
+ *  pool_buffer` and the string length. Therefore, when one request a length
+ *  of a string_ref, its time complexity is O(1).
+ *
+ *  Insertion is a bit costly, but that's expected. We're trading off insertion
+ *  with lookup since in compiler, lookup is the most used operation to find
+ *  duplicated identifier.
+ *
+ *  Note that string pool is only used for identifier, it's not for string
+ *  literal, because it can be large. This is true for C, as string literals
+ *  can be adjacent and they need to be concatenated after preprocessing stage
+ *  and before syntax analysis stage.
+ *
+ *      const char *p = "Hello "
+ *                      "world!"
+ *                      "A very"
+ *                      " long "
+ *                      "string";
+ *
+ */
+
 #define _GNU_SOURCE
 #include <errno.h>
 #include <stdarg.h>
@@ -17,6 +44,7 @@
 #define unlikely(e) e
 #endif
 
+/* TODO: remove/replace this macro */
 #define err_if(e, ...) do {                 \
         if (unlikely((e))) {                \
             fputs("fatal error: ", stderr); \
@@ -26,44 +54,66 @@
         }                                   \
     } while (0)
 
-// ...
+/* ... */
 #define DEFAULT_POOL_CAPA   512u
 #define DEFAULT_ARRAY_CAPA  512u
-#define DEFAULT_BUFFER_SIZE (1u << 18) // 256KiB
+#define DEFAULT_BUFFER_SIZE (1u << 18) /* 256KiB */
 
-// The pool table
-static string_ref *g_pool;
-static unsigned int g_count, g_capacity, g_load_factor;
-
-// This is what a `string_ref` points to
-struct string_off {
-    unsigned int offset;
-    unsigned int length;
+/* The pool table.
+ * Used to implement Set data structure to find duplicated string efficiently.
+ * string_ref of 0 is never stored here.
+ */
+struct pool_table {
+    string_ref *data;
+    unsigned int count;
+    unsigned int capacity;
+    unsigned int load_factor;
 };
-static struct string_off *g_array;
-static unsigned int g_array_count, g_array_capacity;
 
-// The pool buffer
-static char *g_buffer;
-static unsigned int g_used, g_buffer_capacity;
+/* This is what a `string_ref` points to. */
+struct string_off {
+    unsigned int offset; /* Offset in pool_buffer::data */
+    unsigned int length; /* The length (excluding '\0') */
+};
+
+/* We store the information of each string_ref in an array for O(1) access.
+ * There's no need to traverse the array. */
+struct pool_array {
+    struct string_off *data;
+    unsigned int count;
+    unsigned int capacity;
+};
+
+/* A giant mmap()-ed dynamic buffer to store the strings, contiguously.
+ * Each string is guaranteed to be '\0'-terminated.
+ */
+struct pool_buffer {
+    char *data;
+    unsigned int count;
+    unsigned int capacity;
+};
+
+static struct pool_table g_pool;
+static struct pool_array g_array;
+static struct pool_buffer g_buffer;
 
 static void __buffer_setup(void)
 {
-    g_buffer = mmap(NULL,
+    g_buffer.data = mmap(NULL,
                     DEFAULT_BUFFER_SIZE,
                     PROT_READ | PROT_WRITE,
                     MAP_PRIVATE | MAP_ANON,
                     -1, 0);
-    err_if(g_buffer == MAP_FAILED, "unable to allocate string pool: %s",
+    err_if(g_buffer.data == MAP_FAILED, "unable to allocate string pool: %s",
            strerror(errno));
-    g_used = 1; // 0 is reserved for empty string
-    g_buffer_capacity = DEFAULT_BUFFER_SIZE;
+    g_buffer.count = 1; // 0 is reserved for empty string
+    g_buffer.capacity = DEFAULT_BUFFER_SIZE;
 }
 
 static void __buffer_cleanup(void)
 {
-    if (g_buffer)
-        munmap(g_buffer, g_buffer_capacity);
+    if (g_buffer.data != NULL)
+        munmap(g_buffer.data, g_buffer.capacity);
 }
 
 static string_ref __buffer_new(const char *str, unsigned int __size)
@@ -72,37 +122,37 @@ static string_ref __buffer_new(const char *str, unsigned int __size)
     string_ref id = 0;
     unsigned int prev_size, size, capacity;
 
-    buffer = g_buffer;
-    prev_size = g_used;
-    capacity = g_buffer_capacity;
+    buffer = g_buffer.data;
+    prev_size = g_buffer.count;
+    capacity = g_buffer.capacity;
     size = prev_size + __size + 1;
 
     if (size >= capacity) {
         capacity *= 2;
-        buffer = mremap(g_buffer, g_buffer_capacity, capacity, MREMAP_MAYMOVE);
-        err_if(g_buffer == MAP_FAILED, "unable to allocate string pool: %s",
+        buffer = mremap(g_buffer.data, g_buffer.capacity, capacity, MREMAP_MAYMOVE);
+        err_if(g_buffer.data == MAP_FAILED, "unable to allocate string pool: %s",
                strerror(errno));
-        g_buffer = buffer;
-        g_buffer_capacity = capacity;
+        g_buffer.data = buffer;
+        g_buffer.capacity = capacity;
     }
 
     memcpy(buffer + prev_size, str, __size);
-    g_used = size;
+    g_buffer.count = size;
 
-    if (g_array_count >= g_array_capacity) {
-        capacity = g_array_capacity * 2;
-        g_array = realloc(g_array, capacity * sizeof(*g_array));
-        err_if(g_array == NULL, "unable to allocate string pool: %s",
+    if (g_array.count >= g_array.capacity) {
+        capacity = g_array.capacity * 2;
+        g_array.data = realloc(g_array.data, capacity * sizeof(struct string_off));
+        err_if(g_array.data == NULL, "unable to allocate string pool: %s",
                strerror(errno));
-        memset(&g_array[g_array_count],
+        memset(&g_array.data[g_array.count],
                 0,
-               (capacity - g_array_capacity) * sizeof(unsigned int));
-        g_array_capacity = capacity;
+               (capacity - g_array.capacity) * sizeof(unsigned int));
+        g_array.capacity = capacity;
     }
 
-    id = g_array_count++;
-    g_array[id].offset = prev_size;
-    g_array[id].length = __size;
+    id = g_array.count++;
+    g_array.data[id].offset = prev_size;
+    g_array.data[id].length = __size;
     return id;
 }
 
@@ -114,13 +164,13 @@ static uint64_t __do_hash(const char *data, unsigned int len)
 static string_ref __lookup(const char *s0, uint64_t hash, unsigned int len)
 {
     const char *ptr;
-    string_ref s, *strings = g_pool;
-    unsigned int idx, mask = g_capacity - 1;
+    string_ref s, *strings = g_pool.data;
+    unsigned int idx, mask = g_pool.capacity - 1;
 
     idx = hash & mask;
     while ((s = strings[idx]) != 0) {
-        ptr = g_buffer + g_array[s].offset;
-        if (g_array[s].length == len && !memcmp(ptr, s0, len))
+        ptr = g_buffer.data + g_array.data[s].offset;
+        if (g_array.data[s].length == len && !memcmp(ptr, s0, len))
             return s;
         idx = (idx + 1) & mask;
     }
@@ -134,23 +184,23 @@ static void __try_resize(void)
     string_ref *new_pool, *old_pool;
     unsigned int new_capacity, old_capacity, i, idx, mask;
 
-    if (likely(g_count < g_load_factor))
+    if (likely(g_pool.count < g_pool.load_factor))
         return;
 
-    new_capacity = g_capacity * 2;
+    new_capacity = g_pool.capacity * 2;
     mask = new_capacity - 1;
     new_pool = calloc(new_capacity, sizeof(string_ref));
     err_if(new_pool == NULL, "unable to allocate string pool: %s",
            strerror(errno));
 
-    old_pool = g_pool;
-    old_capacity = g_capacity;
+    old_pool = g_pool.data;
+    old_capacity = g_pool.capacity;
 
     for (i = 0; i < old_capacity; i++) {
         string_ref s = old_pool[i];
         if (s != 0) {
-            ptr = g_buffer + g_array[s].offset;
-            idx = __do_hash(ptr, g_array[s].length) & mask;
+            ptr = g_buffer.data + g_array.data[s].offset;
+            idx = __do_hash(ptr, g_array.data[s].length) & mask;
             while (new_pool[idx] != 0)
                 idx = (idx + 1) & mask;
             new_pool[idx] = s;
@@ -158,43 +208,42 @@ static void __try_resize(void)
     }
 
     free(old_pool);
-    g_pool = new_pool;
-    g_capacity = new_capacity;
-    g_load_factor = (unsigned int)((double)new_capacity * 0.80);
+    g_pool.data = new_pool;
+    g_pool.capacity = new_capacity;
+    g_pool.load_factor = (unsigned int)((double)new_capacity * 0.80);
 }
 
 void string_pool_setup(void)
 {
     __buffer_setup();
 
-    g_pool = calloc(DEFAULT_POOL_CAPA, sizeof(string_ref));
-    err_if(g_pool == NULL, "unable to allocate string pool: %s",
+    g_pool.data = calloc(DEFAULT_POOL_CAPA, sizeof(string_ref));
+    err_if(g_pool.data == NULL, "unable to allocate string pool: %s",
            strerror(errno));
-    g_count = 0;
-    g_capacity = DEFAULT_POOL_CAPA;
-    g_load_factor = (unsigned int)((double)(DEFAULT_POOL_CAPA * 0.80));
+    g_pool.capacity = DEFAULT_POOL_CAPA;
+    g_pool.load_factor = (unsigned int)((double)(DEFAULT_POOL_CAPA * 0.80));
 
-    g_array = calloc(DEFAULT_ARRAY_CAPA, sizeof(*g_array));
-    err_if(g_array == NULL, "unable to allocate string pool: %s",
+    g_array.data = calloc(DEFAULT_ARRAY_CAPA, sizeof(struct string_off));
+    err_if(g_array.data == NULL, "unable to allocate string pool: %s",
            strerror(errno));
-    g_array_count = 1; // 0 is reserved for empty string
-    g_array_capacity = DEFAULT_ARRAY_CAPA;
+    g_array.count = 1; // 0 is reserved for empty string
+    g_array.capacity = DEFAULT_ARRAY_CAPA;
 }
 
 void string_pool_cleanup(void)
 {
     __buffer_cleanup();
 
-    free(g_pool);
-    g_pool = NULL;
+    free(g_pool.data);
+    memset(&g_pool, 0, sizeof(g_pool));
 
-    free(g_array);
-    g_array = NULL;
+    free(g_array.data);
+    memset(&g_array, 0, sizeof(g_array));
 }
 
 uint32_t string_pool_count(void)
 {
-    return g_count;
+    return g_pool.count;
 }
 
 string_ref string_ref_newlen(const char *s, unsigned int len)
@@ -202,7 +251,7 @@ string_ref string_ref_newlen(const char *s, unsigned int len)
     string_ref str;
     unsigned int hash, idx, mask;
 
-    if (unlikely(g_pool == NULL))
+    if (unlikely(g_pool.data == NULL))
         string_pool_setup();
 
     if (len == 0)
@@ -215,15 +264,15 @@ string_ref string_ref_newlen(const char *s, unsigned int len)
 
     __try_resize();
 
-    mask = g_capacity - 1;
+    mask = g_pool.capacity - 1;
     idx = hash & mask;
 
-    while (g_pool[idx] != 0)
+    while (g_pool.data[idx] != 0)
         idx = (idx + 1) & mask;
 
     str = __buffer_new(s, len);
-    g_pool[idx] = str;
-    g_count++;
+    g_pool.data[idx] = str;
+    g_pool.count++;
 
     return str;
 }
@@ -240,14 +289,14 @@ string_ref string_ref_concat(string_ref r0, string_ref r1)
     char buf[8192] = {0}, *ptr;
     unsigned int len, len0, len1;
 
-    err_if(r0 >= g_array_capacity, "dangling string_ref (is 0x%08u)", r0);
-    err_if(r1 >= g_array_capacity, "dangling string_ref (is 0x%08u)", r1);
+    err_if(r0 >= g_array.capacity, "dangling string_ref (is 0x%08u)", r0);
+    err_if(r1 >= g_array.capacity, "dangling string_ref (is 0x%08u)", r1);
 
-    len0 = g_array[r0].length;
-    ptr0 = g_buffer + g_array[r0].offset;
+    len0 = g_array.data[r0].length;
+    ptr0 = g_buffer.data + g_array.data[r0].offset;
 
-    len1 = g_array[r1].length;
-    ptr1 = g_buffer + g_array[r1].offset;
+    len1 = g_array.data[r1].length;
+    ptr1 = g_buffer.data + g_array.data[r1].offset;
 
     len = len0 + len1;
     if (len + 1 < sizeof(buf)) {
@@ -268,14 +317,14 @@ string_ref string_ref_concat(string_ref r0, string_ref r1)
 
 const char *string_ref_ptr(string_ref r0)
 {
-    err_if(r0 >= g_array_capacity, "dangling string_ref (is 0x%08u)", r0);
-    return g_buffer + g_array[r0].offset;
+    err_if(r0 >= g_array.capacity, "0x%08u is not valid string_ref", r0);
+    return g_buffer.data + g_array.data[r0].offset;
 }
 
 size_t string_ref_len(string_ref r0)
 {
-    err_if(r0 >= g_array_capacity, "dangling string_ref (is 0x%08u)", r0);
-    return g_array[r0].length;
+    err_if(r0 >= g_array.capacity, "0x%08u is not valid string_ref", r0);
+    return g_array.data[r0].length;
 }
 
 uint64_t string_ref_hash(string_ref r0)
